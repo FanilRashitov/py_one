6e86f97e9922650c43cc4a1614229ebb403528d4
Fanil <rashitov.fanil@yandex.ru>
2024-04-03 16:58:39 +0500

    one

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
-print("Hello World!")
-print("Hello World! 25/03/24")
-print("jgbjhfvhj")
-# =======
-# print("jgbjhfvhj")
-# print("123123123qweqweqwe")
-# >>>>>>> main
+# class First:
+#     pass
+#
+# f = First()
+#
+# print(f)
+# stack = []
+# def push(val):
+#     stack.append(val)
+#
+#
+# def pop():
+#     val = stack[-1]
+#     del stack[-1]
+#     return val
+#
+# push(3)
+# push(2)
+# push(1)
+# print(pop())
+# print(pop())
+# print(pop())
+# class Stack:
+#     def __init__(self):
+#         self.__stackList = []
+#     def push(self, val):
+#         self.__stackList.append(val)
+#     def pop(self):
+#         val = self.__stackList[-1]
+#         del self.__stackList[-1]
+#         return val
+#
+# stackObject1 = Stack()
+# stackObject2 = Stack()
+# stackObject1.push(3)
+#
+# stackObject2.push(stackObject1.pop())
+#
+# stackObject2.add()
+# print(stackObject)
+# class Animal():
+#     def __init__(self):
+#         self.nogi = 0
+#     def walk(self):
+#         print(f"Я хожу на {self.nogi} ногах")
+#
+# class Cat(Animal):
+#     def __init__(self):
+#         Animal.__init__(self)
+#         self.say = "Miya"
+#     def talk(self):
+#         return print(self.say)
+#
+# a = input("Что говорит ваша кошка: ")
+# b = int(input("Сколько раз она это говорит: "))
+# caty = Cat()
+# caty.say = a*b
+# caty.talk()
+# caty.nogi = 4
+# caty.walk()
+
